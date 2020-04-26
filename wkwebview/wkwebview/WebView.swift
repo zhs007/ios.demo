ba8314d7cdedc9e42280bbb7e19c9063cc7a7fbb
zhs007 <sssxueren@gmail.com>
2020-04-26 18:33:55 +0800

    upd

--- wkwebview/wkwebview/WebView.swift
+++ wkwebview/wkwebview/WebView.swift
@@ -32,10 +32,40 @@ class WebView: WKWebView, WKUIDelegate, WKNavigationDelegate {
         }
     }
     
+    func execScriptFile(fn: String) {
+        if let path = Bundle.main.path(forResource: fn, ofType: "js", inDirectory: "assets/js") {
+            let fm = FileManager()
+            let exists = fm.fileExists(atPath: path)
+            if(exists){
+                let content = fm.contents(atPath: path)
+                if let strScript = String(data: content!, encoding: String.Encoding.utf8) {
+                    
+                    evaluateJavaScript(strScript) { (result, error) in
+                        NSLog("exeScriptFile ok")
+                    }
+                }
+            }
+        }
+    }
+    
     func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
         self.load(navigationAction.request)
         
         return nil
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        execScriptFile(fn: "extlib.bundle")
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+//        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
 //    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
